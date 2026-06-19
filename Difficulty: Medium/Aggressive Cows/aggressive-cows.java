b3a2081fddb356660e3731b6f841519cc692bc59
Prabhat Kapkoti <104815270+Dexturn1@users.noreply.github.com>
2026-06-19 15:40:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Aggressive Cows/aggressive-cows.java
+++ Difficulty: Medium/Aggressive Cows/aggressive-cows.java
@@ -3,34 +3,38 @@ class Solution {
         // code here
         Arrays.sort(stalls);
         int low = 1;
-        int high = stalls[stalls.length-1] - stalls[0];
+        int high = stalls[stalls.length - 1] - stalls[0];
         int ans = 0;
         
         while(low <= high){
-            int mid = (low + high) /2;
             
-            if( canPlace(stalls, k, mid) ){
+            int mid = low + (high - low)/2;
+            
+            if(canPlace(stalls, k, mid)){
+                low = mid + 1;  // try bigger distance
                 ans = mid;
-                low = mid+1;
             }
-            else
-                high = mid -1;
+            else{
+                high = mid - 1; // reduce the distance
+            }
         }
         return ans;
+    
     }
     
-    public boolean canPlace(int[] stalls, int cows, int minDist){
+    
+    boolean canPlace(int []stalls, int cows, int minDist){
         int count = 1;
         int lastPlaced = stalls[0];
         
-        for(int i=1; i<stalls.length; i++){
+        for(int i = 1; i < stalls.length; i++){
             if(stalls[i]-lastPlaced >= minDist){
                 count++;
                 lastPlaced = stalls[i];
                 if(count >= cows) return true;
             }
         }
+        
         return false;
     }
-    
 }
