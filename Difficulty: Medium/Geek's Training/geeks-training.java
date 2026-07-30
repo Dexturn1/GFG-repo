c6df9014a2089b1c83e2139237a05f6eab4b6893
Prabhat Kapkoti <104815270+Dexturn1@users.noreply.github.com>
2026-07-30 20:07:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Geek's Training/geeks-training.java
+++ Difficulty: Medium/Geek's Training/geeks-training.java
@@ -1,33 +1,41 @@
 class Solution {
+    int dp[][];
     public int maximumPoints(int mat[][]) {
         // code here
-        int n = mat.length;
-        int [][]dp = new int [n][4];
-        for(int[]row :dp)Arrays.fill(row, -1);
-        return solve(n-1, 3, mat, dp);
+        dp = new int[mat.length][4];
+        for(int[] row: dp){
+            Arrays.fill(row, -1);
+        }
+        return solve(mat.length-1, 3, mat);
     }
     
-    static int solve(int day, int last, int [][]mat, int [][]dp){
-        if(dp[day][last] != -1) return dp[day][last];
+    
+    int solve(int day, int last, int mat[][]){
         
-        if(day == 0){
+        
+        if (dp[day][last] != -1)
+            return dp[day][last];
+        
+       if (day == 0) {
+
             int max = 0;
-            for(int i = 0; i<3; i++){
-                if(i != last){
-                max = Math.max(mat[0][i], max);
+
+            for (int task = 0; task < 3; task++) {
+                if (task != last) {
+                    max = Math.max(max, mat[0][task]);
                 }
             }
-            return dp[day][last] = max;
+
+            return dp[0][last] = max;
         }
-        
+
         int max = 0;
         for(int i = 0; i<3; i++){
             if(i!=last){
-                int point = mat[day][i] + solve(day-1, i, mat, dp);
-                max = Math.max(point, max);
+                max = Math.max(max,solve(day-1, i, mat)+mat[day][i]);
             }
         }
-        
         return dp[day][last] = max;
+        
     }
 }
